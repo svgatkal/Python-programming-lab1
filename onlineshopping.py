fcc83a34353fdc16ee4c9111ee37ef1a2f53fc9d
svgatkal <44358348+svgatkal@users.noreply.github.com>
2018-10-23 18:20:14 +0530

    Add files via upload

---
+++ onlineshopping.py
@@ -0,0 +1,39 @@
+from easygui import *
+import sys
+while 1:
+    msgbox("welcome to online shopping store")
+
+    msg ="types of grocery "
+    title = "currently available options"
+    choices = ["chocalate", "biscuit", "soap", "clothes"]
+    choice = buttonbox(msg, title, choices)
+    if choice=='chocalate':
+      msg1="types of chocalate"
+      title1="available options"
+      choices1=['perk','5 star']
+      choice1=choicebox(msg1,title1,choices1)
+    elif choice=='biscuit':
+      msg1='types of bisciut'
+      title1='available options'
+      choices1=['oreo','parle']
+      choice1=choicebox(msg1,title1,choices1)
+    elif choice=='clothes':
+      msg1='clothes'
+      title1='available options'
+      choices1=['t-shirt','cap','jeans']
+      choice1=choicebox(msg1,title1,choices1)
+    elif choice=='soap':
+         msg1='soap brands'
+         title1='available opyions'
+         choices1=['santoor','dove','patanjali']
+         choice1=choicebox(msg1,title1,choices1) 
+  # note that we convert choice to string, in case
+    # the user cancelled the choice, and we got None.
+    msgbox("You chose: " + str(choice), "selected item")
+
+    msg = "Do you want to continue?"
+    title = "Please Confirm"
+    if ccbox(msg, title):     # show a Continue/Cancel dialog
+        pass  # user chose Continue
+    else:
+        sys.exit(0)

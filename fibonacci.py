89e12abfa7d781a4a5eeccff3d3e88934309461b
svgatkal <44358348+svgatkal@users.noreply.github.com>
2018-12-04 20:58:56 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,19 @@
+nterms=10
+n1=0
+n2=1
+count=0
+if n terms<=0
+  print("please enter positibe number ")
+elif nterms==1:
+  print("fibonecci sequence upto",nterns,":")
+  print(n1)
+else:
+  print("fibonacci sequence upto",nterms,":")
+  while count<nterms:
+    print(n1,end=',')
+    nth=n1+n2
+    #update values
+    n1=n2
+    n2=nth
+    count+=1
+ 

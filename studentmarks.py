6820ab7751d81e9b53c7099c95a42760c20a6270
svgatkal <44358348+svgatkal@users.noreply.github.com>
2018-12-04 22:41:59 +0530

    Add files via upload

---
+++ studentmarks.py
@@ -0,0 +1,10 @@
+name=input("enter your name-")
+rollno=input("enter your roll no-")
+grno=input("enter your gr no-")
+english=input("enter your english marks-")
+maths=input("enter your maths marks-")
+science=input("enter your sci marks-")
+x=["name","roll no","gr no","english","maths","sci"]
+y=[name,rollno,grno,english,maths,science]
+t=dict(zip(x,y))
+print(t)
